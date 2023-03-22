50dacd9534f29913822fd9abdec51f772ac93d9d
Davi Ferreira da Silva <sildv45@gmail.com>
2023-03-22 00:07:04 -0300

    criado novo proj

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String  nome;
+		
+		System.out.println("Boa noite");
+		System.out.println("Digite seu nome");
+		String n = sc.next();
+		System.out.println( n);
+
+	}
+
+}
